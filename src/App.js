86019e6960dcfca8cae35fc7747b81159893fce4
Rob Hess <robwhess@gmail.com>
2021-01-27 15:13:24 -0800

    Add code from 1/27 lecture.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {
+  Route,
+  Switch,
+  Link,
+  NavLink,
+  Redirect,
+  useParams,
+  useRouteMatch
+} from 'react-router-dom';
 
 import './App.css';
 
@@ -22,22 +30,74 @@ function People() {
 }
 
 function Menu() {
-  return <h1>Menu</h1>;
+  const match = useRouteMatch();
+  console.log("== match:", match);
+  const { url, path } = match;
+  return (
+    <>
+      <h1>Menu</h1>
+      <ul>
+        <li><Link to={`${url}/pizza`}>Pizza</Link></li>
+        <li><Link to={`${url}/tacos`}>Tacos</Link></li>
+        <li><Link to={`${url}/sushi`}>Sushi</Link></li>
+      </ul>
+      <Switch>
+        <Route path={`${path}/:menuItem`}>
+          <MenuItem />
+        </Route>
+        <Route exact path={path}>
+          <h2>Please select a menu item above</h2>
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+function MenuItem() {
+  const { menuItem } = useParams();
+  const { url, path } = useRouteMatch();
+  // console.log("== params:", params);
+  return (
+    <div>
+      <h2>Menu item: {menuItem}</h2>
+      <img src={menu[menuItem]} alt={menuItem} />
+      <p>Matched part of the URL: {url}</p>
+      <p>Matched part of the route path: {path}</p>
+    </div>
+  );
 }
 
 function App() {
   return (
-    <Switch>
-      <Route path="/about/people">
-        <People />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/">
-        <Home />
-      </Route>
-    </Switch>
+    <>
+      <ul>
+        <li><NavLink exact to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/people">People</NavLink></li>
+        <li><NavLink to="/menu">Menu</NavLink></li>
+      </ul>
+      <Switch>
+        <Route path="/people">
+          <People />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/home">
+          <Redirect to="/" />
+        </Route>
+        <Route path="/menu">
+          <Menu />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/">
+          <h1>404</h1>
+        </Route>
+      </Switch>
+      <Link to="/">Navigate to home page</Link>
+    </>
   );
 }
 
